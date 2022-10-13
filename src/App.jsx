e35ca22d35d7c8d753e8f73e5fed38487d19802a
Heron Feijao <heronfeijao@gmail.com>
2022-10-13 15:20:35 -0300

    added modal to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Layout/Header.jsx";
 import Meals from "./components/Meals/Meals.jsx";
+import Cart from "./components/Cart/Cart.jsx";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Cart />
       <main>
         <Meals />
       </main>
