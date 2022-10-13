91f272e568ca772701b51d961d075f6f13a90b12
Heron Feijao <heronfeijao@gmail.com>
2022-10-13 15:28:05 -0300

    feat: modal close button

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../UI/Modal.jsx";
 import classes from "./Cart.module.css";
 
 const Cart = () => {
+  const [showCart, setShowCart] = useState(true);
+
+  const toggleModal = () => {
+    showCart ? setShowCart(false) : setShowCart(true);
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
@@ -12,17 +18,23 @@ const Cart = () => {
   );
 
   return (
-    <Modal>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>35.62</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
-        <button className={classes.button}>Order</button>
-      </div>
-    </Modal>
+    <>
+      {showCart && (
+        <Modal>
+          {cartItems}
+          <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>35.62</span>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={toggleModal} className={classes["button--alt"]}>
+              Close
+            </button>
+            <button className={classes.button}>Order</button>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 };
 
