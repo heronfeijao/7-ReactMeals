3e3560b10ecdd76aba807e9a9fbb0b360e635492
Heron Feijao <heronfeijao@gmail.com>
2022-10-13 15:59:06 -0300

    added feat to open and close the modal;

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Header from "./components/Layout/Header.jsx";
 import Meals from "./components/Meals/Meals.jsx";
 import Cart from "./components/Cart/Cart.jsx";
+import { useState } from "react";
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const toggleCart = () => {
+    cartIsShown ? setCartIsShown(false) : setCartIsShown(true);
+  };
+
   return (
     <>
-      <Header />
-      <Cart />
+      {cartIsShown && <Cart onClick={toggleCart} />}
+      <Header onShowCart={toggleCart} />
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,13 +2,7 @@ import React, { useState } from "react";
 import Modal from "../UI/Modal.jsx";
 import classes from "./Cart.module.css";
 
-const Cart = () => {
-  const [showCart, setShowCart] = useState(true);
-
-  const toggleModal = () => {
-    showCart ? setShowCart(false) : setShowCart(true);
-  };
-
+const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
@@ -18,23 +12,19 @@ const Cart = () => {
   );
 
   return (
-    <>
-      {showCart && (
-        <Modal>
-          {cartItems}
-          <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>35.62</span>
-          </div>
-          <div className={classes.actions}>
-            <button onClick={toggleModal} className={classes["button--alt"]}>
-              Close
-            </button>
-            <button className={classes.button}>Order</button>
-          </div>
-        </Modal>
-      )}
-    </>
+    <Modal onClose={props.onClick}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onClick} className={classes["button--alt"]}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
   );
 };
 
